1bbf2ff8608bbbe2de1a06b896b73a110198c620
Spydneo <jairoecalderon@gmail.com>
2022-10-03 20:22:20 +0100

    Añadido test al proyecto

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -1,7 +1,19 @@
-""""""
-# Módulo con helpers para el el preprocesamiento de datos
-""""""
+# """"""
+# # Módulo con helpers para el el preprocesamiento de datos
+# """"""
+# import numpy as np
+
+# def get_numerical_features(df):
+#     return df.select_dtypes(include=[np.number]).columns
+
+# """
+
+# Este módulo contiene helpers para el preprocesamiento de datos
+
+# """
+
 import numpy as np
 
 def get_numerical_features(df):
-    return df.select_dtypes(include=[np.number]).columns
+
+    return list(df.select_dtypes(include=[np.number]).columns)
---
+++ tests/test_preprocessing.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from modeltools.preprocessing import get_numerical_features
+
+def test_get_numerical_features_simple():
+    """
+    Se va a probar que logra distinguir entre cadenas de texto y números enteros
+    assert es “como un if” pero que falla si la condicion
+    es falsa. Esto es ideal para los tests.
+    """
+    df = pd.DataFrame({
+    "numerica": [5],
+    "categorica": ["rojo"]
+    })
+    print("Valor que devuelve la funciòn: --------------")
+    print(get_numerical_features(df))
+    assert get_numerical_features(df) == ['numerica']
+
+
+def test_get_numerical_features_empty():
+
+    """Este test comprueba que se devuelve una lista vacía si no hay ninguna variable num."""
+
+    # EJERCICIO
+    df = pd.DataFrame({
+    "categorica": ["rojo"]
+    })
+
+    assert get_numerical_features(df) == []
+
+
+def test_get_numerical_features_zero_columns():
+
+    """Este test comprueba que se devuelve una lista vacía si el dataframe no tiene ninguna columna."""
+
+    assert get_numerical_features(pd.DataFrame()) == []
+
+
+def test_get_numerical_features_zero_rows():
+
+    """Este test comprueba que se devuelve una con una variable si el DF
+
+    tiene una variable numérica pero NINGUNA FILA."""
+
+    # Filas sin valores del tipo int
+    df = pd.DataFrame( {
+            "numerica": pd.Series(dtype=int)
+        })
+
+    assert get_numerical_features(df) == ['numerica']
+
+
+
+# def test_get_numerical_features_int_and_float():
+
+#     """Este test comprueba que funciona correctamente
+
+#     cuando hay una columna integer y una flotante"""
+
+#     # EJERCICIO
+
+
+# def test_get_numerical_features_complex():
+
+#     """Este test comprueba que funciona correctamente con número complejos."""
+
+#     # EJERCICIO
